d6c2264b5d117240ec013b9005286463a4d9809f
Margaret Berry <codeberry1@gmail.com>
2017-12-01 14:26:34 -0800

    add project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@ Skill.destroy_all
     )
   end
 
-
-
 p "You have created #{Skill.count} skills in your db"
+
+Project.destroy_all
+
+5.times do
+  project = Project.create(
+    title: Faker::FamilyGuy.quote,
+    description: Faker::Hipster.sentences(2),
+    link: Faker::Internet.url
+    )
+  end
+
+p "You have created #{Project.count} projects in your db"
