7f2c435abf66de9cbbf5128e93836c2b12b5500d
Margaret Berry <codeberry1@gmail.com>
2017-12-01 12:24:15 -0800

    add unit tests for skill model

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Skill, 'validation' do
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :example }
+end
+
+describe Skill, 'association' do
+  it { should have_many :endorsements }
+end
+
+describe Skill, 'column_specification' do
+  it { should have_db_column(:description).of_type(:string) }
+  it { should have_db_column(:example).of_type(:string) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
 end
