3f4bfe7716e31a21a6779463e8d4e476c7233bb6
Margaret Berry <codeberry1@gmail.com>
2017-12-01 09:39:13 -0800

    add properties to skill and project classes

--- db/migrate/20171201172703_create_projects.rb
+++ db/migrate/20171201172703_create_projects.rb
@@ -1,6 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+      t.column :title, :string
+      t.column :description, :string
+      t.column :link, :string
 
       t.timestamps
     end
--- db/migrate/20171201172715_create_skills.rb
+++ db/migrate/20171201172715_create_skills.rb
@@ -1,6 +1,8 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
+      t.column :description, :string
+      t.column :example, :string
 
       t.timestamps
     end
