7c6b344407dcadf4ec6bc59a72765046dd7717a7
Margaret Berry <codeberry1@gmail.com>
2017-12-01 13:51:14 -0800

    add admin authorization for projects and skills

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,7 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index, :show]
+
+  before_action :only => [:new, :edit] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,10 @@
 class SkillsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index, :show]
+
+  before_action :only => [:new, :edit] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
+
   def index
     @skills = Skill.all
   end
