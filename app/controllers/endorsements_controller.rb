8c3042c77d0ed00bf5928d2dbe60373f3beba672
Margaret Berry <codeberry1@gmail.com>
2017-12-01 15:36:41 -0800

    add CRUD functionality for endorsements

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,2 +1,44 @@
 class EndorsementsController < ApplicationController
+  def new
+    @skill = Skill.find(params[:skill_id])
+    @endorsement = Endorsement.new
+  end
+
+  def create
+    @skill = Skill.find(params[:skill_id])
+    @endorsement = @skill.endorsements.new(endorsement_params)
+    if @endorsement.save
+      flash[:notice] = "Endorsement successfully added!"
+      redirect_to skill_path(@skill)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @endorsement = Endorsement.find(params[:id])
+    @skill = @endorsement.skill
+  end
+
+  def update
+    @endorsement = Endorsement.find(params[:id])
+    if @endorsement.update(endorsement_params)
+      flash[:notice] = "Endorsement successfully updated!"
+      redirect_to skill_path(@endorsement.skill)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @endorsement = Endorsement.find(params[:id])
+    @endorsement.destroy
+    flash[:notice] = "Endorsement successfully deleted!"
+    redirect_to skill_path(@endorsement.skill)
+  end
+
+  private
+    def endorsement_params
+      params.require(:endorsement).permit(:body, :skill_id, :user_id)
+    end
 end
