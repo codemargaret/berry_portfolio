50301ee4a6fa128897c61f2dceecdcc8b4628ceb
Margaret Berry <codeberry1@gmail.com>
2017-12-01 12:13:52 -0800

    add relationships and validations to skills and endorsements

--- app/models/endorsement.rb
+++ app/models/endorsement.rb
@@ -1,2 +1,8 @@
 class Endorsement < ApplicationRecord
+  belongs_to :skill
+  belongs_to :user
+
+  validates :user_id, :presence => true
+  validates :skill_id, :presence => true
+  validates :body, :presence => true
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,6 @@
 class Skill < ApplicationRecord
+  has_many :endorsements
+
+  validates :description, :presence => true
+  validates :example, :presence => true
 end
