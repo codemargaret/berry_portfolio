b43712c76bfec09b09ab89ae4df7c208d51e4856
Margaret Berry <codeberry1@gmail.com>
2017-12-01 14:35:39 -0800

    add link to projects page in navbar, create project index and show pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,13 @@ class ProjectsController < ApplicationController
   before_action :only => [:new, :edit] do
     redirect_to new_user_session_path unless current_user && current_user.admin
   end
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @comments = @project.comments
+  end
 end
