4e39ad3cd1479ce8847ac974717a843905746db9
Margaret Berry <codeberry1@gmail.com>
2017-12-01 12:27:57 -0800

    add unit tests for project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Project, 'validation' do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :link }
+end
+
+describe Project, 'association' do
+  it { should have_many :comments }
+end
+
+describe Project, 'column_specification' do
+  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:description).of_type(:string) }
+  it { should have_db_column(:link).of_type(:string) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
 end
